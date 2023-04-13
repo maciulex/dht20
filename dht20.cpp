14054c7948bd9b9e4867e5d2363a0ceb30a7217e
maciulex <41756057+maciulex@users.noreply.github.com>
2023-04-13 18:13:39 +0200

    everything done

--- dht20.cpp
+++ dht20.cpp
@@ -1,6 +1,23 @@
+#ifndef DHT20
+#define DHT20
+
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+
+#ifndef INT_TO_BIN
+#define INT_TO_BIN
+    #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+    #define BYTE_TO_BINARY(byte)  \
+    (byte & 0x80 ? '1' : '0'), \
+    (byte & 0x40 ? '1' : '0'), \
+    (byte & 0x20 ? '1' : '0'), \
+    (byte & 0x10 ? '1' : '0'), \
+    (byte & 0x08 ? '1' : '0'), \
+    (byte & 0x04 ? '1' : '0'), \
+    (byte & 0x02 ? '1' : '0'), \
+    (byte & 0x01 ? '1' : '0') 
+#endif
 
 #ifndef I2C_INTERFACE
     #define I2C_INTERFACE 
@@ -14,6 +31,9 @@
 namespace DHT20 {
     uint8_t i2c_address = 0b0011'1000;
     uint8_t read_bit    = 0b1000'0000;
+    
+    double result[2] = {0x00, 0x00};
+    bool print = false;
 
     void I2Cinit() {
         i2c_init(I2C_PORT, I2C_CLOCK);
@@ -40,20 +60,17 @@ namespace DHT20 {
         sleep_ms(80);
         while (true) {
             status = getStatusWord();
-            sleep_ms(1000);
-            printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(status));
-            printf("\n");
-            printf("\n");
+            sleep_ms(50);
             if (!(status & 0x80)) break;
         }
 
         uint8_t data[7];
         i2c_read_blocking(I2C_PORT, i2c_address | read_bit, data, 7, false);
         for (int i = 0; i < 7; i++) {
+            if (!print) break;
             printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(data[i]));
             printf("\n");
         }
-        printf("\n");
         double srh = (uint32_t)data[1] << 12 | (uint32_t)data[2] << 4 | (uint32_t)data[3] >> 4;
         srh = srh/1048576;
         srh *= 100;
@@ -61,23 +78,25 @@ namespace DHT20 {
         tmp = tmp/1048576;
         tmp *= 200;
         tmp -= 50;
-        printf("TEMP: ");
-        printf("%f", tmp);
-        printf("SRH: ");
-        printf("%f", srh);
+        result[0] = tmp;
+        result[1] = srh;
     }
 
     void fullINIT() {
         uint8_t status;
         I2Cinit();  
         status = getStatusWord();
-        printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(status));
-        printf("\n");
+        if (print){
+            printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(status));
+            printf("\n");
+        }
         sleep_ms(40);
         while (true) {
             status = getStatusWord();
-            printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(status));
-            printf("\n");
+            if (print){
+                printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(status));
+                printf("\n");
+            }
             if (status & 0x08) {
                 break;
             }
@@ -89,3 +108,5 @@ namespace DHT20 {
     }
 
 }
+
+#endif
--- main.cpp
+++ main.cpp
@@ -2,16 +2,19 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)  \
-  (byte & 0x80 ? '1' : '0'), \
-  (byte & 0x40 ? '1' : '0'), \
-  (byte & 0x20 ? '1' : '0'), \
-  (byte & 0x10 ? '1' : '0'), \
-  (byte & 0x08 ? '1' : '0'), \
-  (byte & 0x04 ? '1' : '0'), \
-  (byte & 0x02 ? '1' : '0'), \
-  (byte & 0x01 ? '1' : '0') 
+#ifndef INT_TO_BIN
+#define INT_TO_BIN
+    #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+    #define BYTE_TO_BINARY(byte)  \
+    (byte & 0x80 ? '1' : '0'), \
+    (byte & 0x40 ? '1' : '0'), \
+    (byte & 0x20 ? '1' : '0'), \
+    (byte & 0x10 ? '1' : '0'), \
+    (byte & 0x08 ? '1' : '0'), \
+    (byte & 0x04 ? '1' : '0'), \
+    (byte & 0x02 ? '1' : '0'), \
+    (byte & 0x01 ? '1' : '0') 
+#endif
 
 
 #include "dht20.cpp"
